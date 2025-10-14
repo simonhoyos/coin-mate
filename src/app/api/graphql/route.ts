5b2064b54b9453b4fb9afd0cac2562ec33038711
Simon Hoyos <shmesa23@gmail.com>
2025-10-14 14:48:03 -0500

    Fix server types

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,7 +1,8 @@
-import { ApolloServer } from "@apollo/server";
-import { ApolloServerPluginLandingPageDisabled } from "@apollo/server/plugin/disabled";
-import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
-import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import type { NextRequest } from 'next/server';
 
 const baseTypeDefs = `#graphql
   scalar UUID
@@ -19,17 +20,22 @@ const server = new ApolloServer({
   typeDefs: [baseTypeDefs],
   resolvers: [],
   plugins: [
-    process.env.NODE_ENV === "production"
+    process.env.NODE_ENV === 'production'
       ? ApolloServerPluginLandingPageDisabled()
       : ApolloServerPluginLandingPageLocalDefault({ footer: false }),
   ],
 });
 
-const handler = startServerAndCreateNextHandler(server, {
-  context: async (req, res) => ({
+const handler = startServerAndCreateNextHandler<NextRequest>(server, {
+  context: async (req) => ({
     req,
-    res,
   }),
 });
 
-export { handler as GET, handler as POST };
+export async function GET(request: NextRequest) {
+  return handler(request);
+}
+
+export async function POST(request: NextRequest) {
+  return handler(request);
+}
