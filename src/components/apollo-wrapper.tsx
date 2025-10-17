558ff91059ac607b36a97fc8a2599780bf8e457d
Simon Hoyos <shmesa23@gmail.com>
2025-10-17 06:37:34 -0500

    Configure client side apollo client

---
+++ src/components/apollo-wrapper.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { HttpLink } from '@apollo/client';
+import { ApolloClient, ApolloNextAppProvider, InMemoryCache } from '@apollo/client-integration-nextjs';
+
+export function makeApolloClient() {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+
+    link: new HttpLink({
+      uri: `/api/graphql`,
+      fetchOptions: { cache: 'no-store' },
+      credentials: 'same-origin',
+    }),
+  });
+}
+
+export function ApolloWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ApolloNextAppProvider makeClient={makeApolloClient}>
+      {children}
+    </ApolloNextAppProvider>
+  );
+}
