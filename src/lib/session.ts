3d17bdf8d9a75af582d8698f93bab12084f13aec
Simon Hoyos <shmesa23@gmail.com>
2026-07-19 18:06:34 -0500

    fix(session): change cookie name

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -6,7 +6,7 @@ const MS_IN_A_DAY = 24 * 60 * 60 * 1000;
 export const SESSION_DURATION_MS = 7 * MS_IN_A_DAY;
 export const REFRESH_THRESHOLD_MS = 1 * MS_IN_A_DAY;
 
-const SESSION_COOKIE_NAME = 'session';
+const SESSION_COOKIE_NAME = 'cmsession';
 
 export async function createSession(token: string, expiresAt?: Date) {
   const sessionExpiresAt =
